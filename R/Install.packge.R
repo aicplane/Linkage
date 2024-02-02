1d3e4c6a9da71dd5c909fcf845fe11d3b1912e30
aicplane <2470587020@qq.com>
2024-02-02 16:22:21 +0800

    update

--- R/Install.packge.R
+++ R/Install.packge.R
@@ -2,42 +2,34 @@ Install.biocViews <- function() {
   if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("TFBSTools", quietly = TRUE))
     BiocManager::install("TFBSTools")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("motifmatchr", quietly = TRUE))
     BiocManager::install("motifmatchr")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("Gviz", quietly = TRUE))
     BiocManager::install("Gviz")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("ChIPseeker", quietly = TRUE))
     BiocManager::install("ChIPseeker")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("TxDb.Hsapiens.UCSC.hg38.knownGene", quietly = TRUE))
     BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("TxDb.Mmusculus.UCSC.mm10.knownGene", quietly = TRUE))
     BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE))
     BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("BSgenome.Mmusculus.UCSC.mm10", quietly = TRUE))
     BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("org.Hs.eg.db", quietly = TRUE))
     BiocManager::install("org.Hs.eg.db")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
+  if (!requireNamespace("org.Mm.eg.db", quietly = TRUE))
     BiocManager::install("org.Mm.eg.db")
   
-  if (!requireNamespace("BiocManager", quietly = TRUE))
-    BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
-  
-  if (!requireNamespace("BiocManager", quietly = TRUE))
-    BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
-  
-  if (!requireNamespace("BiocManager", quietly = TRUE))
-    BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
 }
