6d3783eb5cefc21290371b284a064033a675ec5c
aicplane <2470587020@qq.com>
2024-02-06 11:21:23 +0800

    test

--- tests/test.R
+++
@@ -1,5 +0,0 @@
-Linkage <- function() {
-  appDir <- system.file(paste0("apps"), package = "Linkage")
-  if (appDir == "") stop("The shiny app ", name, " does not exist")
-  shiny::runApp(appDir)
-}
