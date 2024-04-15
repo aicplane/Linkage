162703d4cdd3941d593b04bf461cda7e247530c8
aicplane <2470587020@qq.com>
2024-04-15 15:10:36 +0800

    DATA

---
+++ R/data.R
@@ -0,0 +1,98 @@
+#' Homo sapiens genetic coordinates
+#'
+#' Used to find the coordinates of each gene
+#' Report ...
+#'
+#' @format ## `Homo.position`
+#' A data frame with 66,206 rows and 6 columns:
+#' \describe{
+#'   \item{ensembl_gene_id, external_gene_name, entrezgene_id}{Gene ID type}
+#'   \item{chromosome_name}{chromosome name}
+#'   \item{start_position}{chromosome start site}
+#'   \item{end_position}{chromosome termination site}
+#' }
+"Homo.position"
+
+
+#' Mus musculus sapiens genetic coordinates
+#'
+#' Used to find the coordinates of each gene
+#' Report ...
+#'
+#' @format ## `Mus.position`
+#' A data frame with 57,451 rows and 6 columns:
+#' \describe{
+#'   \item{ensembl_gene_id, external_gene_name, entrezgene_id}{Gene ID type}
+#'   \item{chromosome_name}{chromosome name}
+#'   \item{start_position}{chromosome start site}
+#'   \item{end_position}{chromosome termination site}
+#' }
+"Mus.position"
+
+
+#' Homo sapiens genetic coordinates
+#'
+#' Used to find the coordinates of each gene
+#' Report ...
+#'
+#' @format ## `Homo.position`
+#' A data frame with 66,206 rows and 6 columns:
+#' \describe{
+#'   \item{ensembl_gene_id, external_gene_name, entrezgene_id}{Gene ID type}
+#'   \item{chromosome_name}{chromosome name}
+#'   \item{start_position}{chromosome start site}
+#'   \item{end_position}{chromosome termination site}
+#' }
+"Homo.position"
+
+
+#' Homo sapiens PWM matrix list of motif
+#'
+#' include 692 motifs
+#'
+#' Report ...
+#'
+#' @format ## `PFMatrixList`
+"PFMatrixList"
+
+#' Mus musculus sapiens PWM matrix list of motif
+#'
+#' include 144 motifs
+#'
+#' Report ...
+#'
+#' @format ## `Mus.PFMatrixList`
+"Mus.PFMatrixList"
+
+#' Homo sapiens motif id and name
+#'
+#' Report ...
+#'
+#' @format ## `pwm_library_dt`
+#' A data frame with 692 rows and 2 columns:
+#' \describe{
+#'   \item{ID}{motif id}
+#'   \item{name}{motif name}
+#' }
+"pwm_library_dt"
+
+
+#' Mus musculus sapiens motif id and name
+#'
+#' Report ...
+#'
+#' @format ## `Mus.pwm_library_dt`
+#' A data frame with 144 rows and 2 columns:
+#' \describe{
+#'   \item{ID}{motif id}
+#'   \item{name}{motif name}
+#' }
+"Mus.pwm_library_dt"
+
+
+#' An small LinkageObject with 5 genes and 3982 peaks
+#'
+#' Report ...
+#'
+#' @format ## `SmallLinkageObject`
+"SmallLinkageObject"
